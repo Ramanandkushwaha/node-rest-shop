ebf82879c92ec64bd6e86381f4a07a9fb32220d4
Ramanandkushwaha <anand.kushwaha49@gmail.com>
2018-07-23 01:02:25 +0530

    Mongodb and Mongoose database connection

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require("../models/product");
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,15 +11,46 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
+    });
+
+    product
+        .save().then(result => {
+            console.log(result);
+        })
+        .catch(err => console.log(err));
+
     res.status(201).json({
         message: 'Handling POST request to /products',
         createProduct: product
     });
 });
+
+// router.post("/", (req, res, next) => {
+//   const product = new Product({
+//     _id: new mongoose.Types.ObjectId(),
+//     name: req.body.name,
+//     price: req.body.price
+//   });
+//   product
+//     .save()
+//     .then(result => {
+//       console.log(result);
+//       res.status(201).json({
+//         message: "Handling POST requests to /products",
+//         createdProduct: result
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({
+//         error: err
+//       });
+//     });
+// });
 
 router.patch('/', (req, res, next) => {
     res.status(200).json({
--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 //routes which should handle requests
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+  "mongodb://node-shop:" +
+    process.env.MONGO_ATLAS_PW +
+    "@node-rest-shop-shard-00-00-wovcj.mongodb.net:27017,node-rest-shop-shard-00-01-wovcj.mongodb.net:27017,node-rest-shop-shard-00-02-wovcj.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin",
+  {
+    useMongoClient: true
+  }
+);
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -18,7 +29,7 @@ app.use((req, res, next) => {
 		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
 	);
 	if(req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
 		return res.status(200).json({});
 	}
 	next();
