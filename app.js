46ff1fc145815d4e90c300b5f1448d9b89e52098
Ramanandkushwaha <anand.kushwaha49@gmail.com>
2018-07-24 01:28:56 +0530

    mongoose database successfully connected

--- app.js
+++ app.js
@@ -8,14 +8,25 @@ const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-mongoose.connect(
-  "mongodb://node-shop:" +
-    process.env.MONGO_ATLAS_PW +
-    "@node-rest-shop-shard-00-00-wovcj.mongodb.net:27017,node-rest-shop-shard-00-01-wovcj.mongodb.net:27017,node-rest-shop-shard-00-02-wovcj.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin",
-  {
-    useMongoClient: true
-  }
-);
+// mongoose.connect(
+//   "mongodb://node-shop:" +
+//   process.env.MONGO_ATLAS_PW +
+//   "@node-rest-shop-shard-00-00-j92m6.mongodb.net:27017,node-rest-shop-shard-00-01-j92m6.mongodb.net:27017,node-rest-shop-shard-00-02-j92m6.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true",
+//   {
+//     useMongoClient: true
+//   }
+// );
+
+mongoose.connect("mongodb://localhost:27017/nodeRestShop", { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("Database Connected!");
+});
+
+exports.nodeRestShop = function(req,res) {
+  res.render('nodeRestShop');
+};
 
 
 app.use(morgan('dev'));
